7e7d57096e19e4605a0a7e3947854ae479a7021c
fschwiet <fschwiet@gmail.com>
2013-03-30 17:26:05 -0700

    draw skeleton

--- HelloKinect/DrawSkeletonCommand.cs
+++ HelloKinect/DrawSkeletonCommand.cs
@@ -31,48 +31,107 @@ namespace HelloKinect
 
         protected override void SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
         {
-            using (var skeleton = e.OpenSkeletonFrame())
+            try
             {
-                Skeleton[] data = new Skeleton[skeleton.SkeletonArrayLength];
-                skeleton.CopySkeletonDataTo(data);
-
-                var skele = data.FirstOrDefault();
-
-                if (skele == null)
-                    return;
-
-                if (skele.TrackingState != SkeletonTrackingState.Tracked)
+                using (var skeleton = e.OpenSkeletonFrame())
                 {
-                    Console.Write(".");
-                    return;
-                }
+                    Skeleton[] data = new Skeleton[skeleton.SkeletonArrayLength];
+                    skeleton.CopySkeletonDataTo(data);
 
-                using (var graphics = _displayForm.CreateGraphics())
-                {
-                    graphics.Clear(Color.Black);
+                    var skele = data.FirstOrDefault();
 
-                    foreach (var jointType in Enum.GetValues(typeof(JointType)).Cast<JointType>())
+                    if (skele == null)
+                        return;
+
+                    if (skele.TrackingState != SkeletonTrackingState.Tracked)
                     {
-                        Joint joint = skele.Joints[jointType];
+                        Console.Write(".");
+                        return;
+                    }
 
-                        Color? color = null;
+                    using (var graphics = _displayForm.CreateGraphics())
+                    {
+                        graphics.Clear(Color.Black);
 
-                        switch (joint.TrackingState)
-                        {
-                            case JointTrackingState.Tracked:
-                                color = Color.Aqua;
-                                break;
-                            case JointTrackingState.Inferred:
-                                color = Color.Orange;
-                                break;
+                        var jointTypes = Enum.GetValues(typeof (JointType)).Cast<JointType>();
 
-                        }
+                        var points = ExtractScreenPoints(jointTypes, skele);
 
-                        var position = _sensor.CoordinateMapper.MapSkeletonPointToDepthPoint(joint.Position, DepthImageFormat.Resolution640x480Fps30);
-                        graphics.DrawString(jointType.ToString(), new Font(FontFamily.GenericSansSerif, 12), new SolidBrush(color.Value), position.X, position.Y);
+                        DrawJointConnections(jointTypes, points, graphics);
+
+                        DrawJoints(points, graphics);
                     }
                 }
             }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+        }
+
+        private static void DrawJoints(Dictionary<JointType, Tuple<Color?, DepthImagePoint>> points, Graphics graphics)
+        {
+            foreach (var point in points)
+            {
+                if (point.Value.Item1.HasValue)
+                {
+                    graphics.FillEllipse(new SolidBrush(point.Value.Item1.Value), point.Value.Item2.X, point.Value.Item2.Y, 5,5);
+                }
+            }
+        }
+
+        private static void DrawJointConnections(IEnumerable<JointType> jointTypes, Dictionary<JointType, Tuple<Color?, DepthImagePoint>> points, Graphics graphics)
+        {
+            var grayPen = new Pen(Color.Gray);
+
+            foreach (var startJoint in jointTypes)
+            {
+                if (!JointStructure.Connections.ContainsKey(startJoint))
+                    continue;
+
+                if (!points[startJoint].Item1.HasValue)
+                    continue;
+
+                var startPoint = points[startJoint].Item2;
+
+                foreach (var endJoint in JointStructure.Connections[startJoint])
+                {
+                    if (!points[endJoint].Item1.HasValue)
+                        continue;
+
+                    var endPoint = points[endJoint].Item2;
+
+                    graphics.DrawLine(grayPen, startPoint.X, startPoint.Y, endPoint.X, endPoint.Y);
+                }
+            }
+        }
+
+        private Dictionary<JointType, Tuple<Color?, DepthImagePoint>> ExtractScreenPoints(IEnumerable<JointType> jointTypes, Skeleton skele)
+        {
+            Dictionary<JointType, Tuple<Color?, DepthImagePoint>> points =
+                new Dictionary<JointType, Tuple<Color?, DepthImagePoint>>();
+
+            foreach (var jointType in jointTypes)
+            {
+                Joint joint = skele.Joints[jointType];
+
+                Color? color = null;
+
+                switch (joint.TrackingState)
+                {
+                    case JointTrackingState.Tracked:
+                        color = Color.Aqua;
+                        break;
+                    case JointTrackingState.Inferred:
+                        color = Color.Orange;
+                        break;
+                }
+
+                DepthImagePoint position = _sensor.CoordinateMapper.MapSkeletonPointToDepthPoint(
+                    joint.Position, DepthImageFormat.Resolution640x480Fps30);
+                points.Add(jointType, new Tuple<Color?, DepthImagePoint>(color, position));
+            }
+            return points;
         }
     }
 }
---
+++ HelloKinect/JointStructure.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using Microsoft.Kinect;
+
+namespace HelloKinect
+{
+    public class JointStructure
+    {
+        public static Dictionary<JointType, IEnumerable<JointType>> Connections = new Dictionary<JointType, IEnumerable<JointType>>()
+        {
+            {JointType.HipCenter, new [] { JointType.Spine, JointType.HipLeft, JointType.HipRight}},
+                {JointType.Spine, new [] { JointType.ShoulderCenter}},
+                    {JointType.ShoulderCenter, new [] { JointType.ShoulderLeft, JointType.Head,JointType.ShoulderRight, }},
+                        {JointType.ShoulderLeft, new [] { JointType.ElbowLeft, }},
+                        {JointType.ShoulderRight, new [] { JointType.ElbowRight, }},
+                            {JointType.ElbowLeft, new [] { JointType.WristLeft, }},
+                            {JointType.ElbowRight, new [] { JointType.WristRight, }},
+                                {JointType.WristLeft, new [] { JointType.HandLeft, }},
+                                {JointType.WristRight, new [] { JointType.HandRight, }},
+                {JointType.HipLeft, new [] {JointType.KneeLeft, }},
+                {JointType.HipRight, new [] {JointType.KneeRight, }},
+                    {JointType.KneeLeft, new [] {JointType.AnkleLeft, }},
+                    {JointType.KneeRight, new [] {JointType.AnkleRight, }},
+                        {JointType.AnkleLeft, new [] {JointType.FootLeft, }},
+                        {JointType.AnkleRight, new [] {JointType.FootRight, }},
+
+        };
+    }
+}
